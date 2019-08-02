b504c7c6d5747db9bd64741bf845f89dbabb9620
nayamama@hotmail.com <nayamama@hotmail.com>
2019-08-01 21:40:29 -0400

    add failsafe plugin and test file for integration test.

---
+++ src/test/java/com/my_java_app/JavaHelloIT.java
@@ -0,0 +1,10 @@
+package com.my_java_app;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloIT {
+    @Test
+    void myFakeIntegrationTest(){
+        System.out.println("IT is running...");
+    }
+}
